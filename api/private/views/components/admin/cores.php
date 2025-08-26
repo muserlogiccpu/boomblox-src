2efd24c998f93c4835585905815b3528849d4803
marsoc <mlgmusic445@gmail.com>
2025-08-26 17:43:58 -0500

    subtle admin panel updates

--- api/private/views/components/admin/cores.php
+++ api/private/views/components/admin/cores.php
@@ -1,6 +1,13 @@
 <div id="MainPanel">
     <div>
+        <h1>Machines & Cores</h1>
+        <hr>
+        <h1>Cores</h1>
         <br>Cores: <?=CPU::getCpuUsagePct()?>% in use of <?=CPU::getCPUs()?><br>
         <br><?=Gameservers::countRunning()?> running, 0 waiting
+        <hr>
+        <h1>Machines (1)</h1>
+        <p>104.219.236.150</p>
+        <p>New York, USA</p>
     </div>
 </div>
