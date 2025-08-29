eed93507a7848bc5c5c1f501678e93a9631c8b3c
marsoc <mlgmusic445@gmail.com>
2025-08-29 17:10:08 -0500

    Added welcome message that gets sent to new users

--- api/private/classes/registration.php
+++ api/private/classes/registration.php
@@ -52,6 +52,20 @@ class Registration {
         $db->createUser($_POST["Username"],$_POST["Password"],$_POST["Key"]);
         $id = $db->getIdByUser($_POST["Username"]);
         ROBLOSECURITY::new($id);
+
+        $newUser = new User($id);
+        $message = [
+            "senderId" => 1,
+            "senderUn" => "Boomblox",
+            "subject" => "Welcome to Boomblox",
+            "content" => "Hello, new Boombloxian!!
+
+Welcome to Boomblox! We are constantly working to make Boomblox a fun, safe, and creative place for everyone. We update Boomblox frequently, so be sure to visit our NEWS section to learn about new exciting updates. If you have questions about how something works, our HELP section is a great place to start. Finally, the FORUM is a where you can find other people and make friends. Be sure to read the Community Guidelines as well, so you know how best to get along with people on Boomblox! http://wiki.roblox.com/index.php?title=Community_Guidelines
+
+Have a great time here!";
+        ];
+        $newUser->sendMessage($message);
+
         return true;
     }
     public function error($json, $focus) {
