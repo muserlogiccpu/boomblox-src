93d8b7f53b75e954c8da02b8388cbfec006a6faa
marsoc <boombloxkewl@gmail.com>
2025-09-09 20:59:20 -0500

    documented CPU class

--- api/private/classes/cpu.php
+++ api/private/classes/cpu.php
@@ -10,30 +10,48 @@ class CPU {
 
     # how much ram (in bytes) we have
     private static int $ram = 32000000;
+
+    # converts bytes to megabytes
     public static function toMB(int $size): float|int {
         return $size/1000;
     }
+
+    # converts bytes to gigabytes
     public static function toGB(int $size): float|int {
         return $size/1000000;
     }
+
+    # gets the percentage of how much stress there is on the cpus
     public static function getCpuUsagePct() {
         return self::computePct(self::getUsage()/self::$cpuCount);
     }
+
+    # gets the percentage of how much stress there is on the machines
     public static function getMachineUsagePct() {
         return self::computePct(self::getUsage());
     }
+
+    # computes a percentage
     public static function computePct($pct) {
         return ceil($pct*1000)/1000;
     }
+
+    # returns count of machines
     public static function getMachines() {
         return self::$machineCount;
     }
+
+    # returns count of cpus
     public static function getCPUs() {
         return self::$cpuCount;
     }
+
+    # returns ram size
     public static function getRam() {
         return self::$ram;
     }
+
+    # saves cpu usage info to cpulog.txt
     public static function saveUsage() {
         exec('typeperf "\Processor(_Total)\% Processor Time" -sc 1', $output);
         if (isset($output[2]) && preg_match('/"([\d.]+)"/', $output[2], $matches)) {
@@ -43,6 +61,8 @@ class CPU {
             fclose($file);
         }
     }
+
+    # gets current usage from cpulog.txt
     public static function getUsage() {
         if (file_exists($_SERVER["DOCUMENT_ROOT"]."/api/private/core/cpulog.txt")) {
             $file = $_SERVER["DOCUMENT_ROOT"]."/api/private/core/cpulog.txt";
