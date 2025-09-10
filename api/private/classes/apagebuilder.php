93ff8d040928850e867d110cfe6c9cb55c0ff28a
marsoc <boombloxkewl@gmail.com>
2025-09-09 20:41:58 -0500

    documented PageBuilder class

--- api/private/classes/apagebuilder.php
+++ api/private/classes/apagebuilder.php
@@ -13,11 +13,18 @@ if (!$auth->hasPerms(3)) {
     Server::_404();  
 }
 
+# admin page builder, since there is a completely different page scheme
 class APageBuilder {
+
+    # defines current user theme
     private $theme;
+
+    # main constructor
     public function __construct($theme = 0) {
         $this->theme = $theme;
     }
+
+    # builds the admin panel header
     public function buildHeader($default = true, $dashboard = true) {
         $theme = $this->theme;
         include_once $_SERVER['DOCUMENT_ROOT'] . "/aaa/components/head.php";
@@ -29,6 +36,8 @@ class APageBuilder {
             }
         }
     }
+
+    # builds the admin panel footer
     public function buildFooter() {
         include_once $_SERVER['DOCUMENT_ROOT'] . "/aaa/components/adminend.php";
     }
