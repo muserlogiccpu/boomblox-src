c2d2e0959d5ae9c3052224371c80adc1b374bf58
marsoc <boombloxkewl@gmail.com>
2025-09-09 20:36:33 -0500

    added documentation for Ad class

--- api/private/classes/ad.php
+++ api/private/classes/ad.php
@@ -1,12 +1,15 @@
 <?php
+# for ad generation on-site
 class Ad {
+
+    # generates an ad
     public static function generateAd($size) {
         switch ($size) {
             case "160x600":
                 $rand = mt_rand(1,3);
                 $adName = "googleads";
                 include_once($_SERVER["DOCUMENT_ROOT"]."/aaa/components/ad.php");
-                break;
+            break;
         }
     }
 }
