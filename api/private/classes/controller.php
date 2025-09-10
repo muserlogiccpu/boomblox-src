4233daba92682f86f30086181c66e66c0ff64639
marsoc <boombloxkewl@gmail.com>
2025-09-09 20:56:37 -0500

    documented Controller extendable class

--- api/private/classes/controller.php
+++ api/private/classes/controller.php
@@ -1,5 +1,7 @@
 <?php
+# sole controller class
 class Controller {
+    # requires authentication
     public static function requireAuth() {
         global $auth;
         !$auth->isAuthed() && header("HTTP/1.1 404 Not Found");
