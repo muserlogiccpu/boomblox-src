658aba9fa8a9c040f4724a87abb09b475bb8264e
marsoc <boombloxkewl@gmail.com>
2025-09-17 21:09:32 -0500

    completed economy.js estimations

--- api/private/js/economy.js
+++ api/private/js/economy.js
@@ -10,4 +10,16 @@ class EconomyServices {
             onError(error, context);
         }
     }
+
+    static GetEstimatedTradeReturnForTickets(amountToTrade, onBux, onError, context) {
+        try {
+            if (typeof Number(amountToTrade) !== "number" || amountToTrade < 10) {
+                throw new Error("Invalid trade amount");
+            }
+
+            onBux(Math.floor(Number(amountToTrade / 10)), context);
+        } catch (error) {
+            onError(error, context);
+        }
+    }
 }
