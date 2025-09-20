abd362a12b0a5f0148b607b2a93ae90b8a9438e0
marsoc <boombloxkewl@gmail.com>
2025-09-19 21:48:35 -0500

    update model data saving

--- api/public/SaveData.php
+++ api/public/SaveData.php
@@ -2,7 +2,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
 
 // EXAMPLE:
-// game:HttpGet("http://bmblox.xyz/api/public/SaveData.ashx?key=ABCD-1234-EFGH-5678&data=[user=3;coins=100]")
+// game:HttpGet("http://xoblog.dev/api/public/SaveData.ashx?key=ABCD-1234-EFGH-5678&data=[user=3;coins=100]")
 
 $privateKey = $_GET["key"];
 if (Datastore::keyExists($privateKey)) {
