96175317174e4df7e187984489c4b3339b335425
marsoc <boombloxkewl@gmail.com>
2025-09-24 19:23:44 -0500

    xp client download added to site for xp testers

--- Install/ActiveDownload.php
+++ Install/ActiveDownload.php
@@ -1,25 +1,34 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
-global $auth;
+global $auth, $user;
+
+!$auth->isAuthed() && Server::_404();
 
 $file = $_SERVER["DOCUMENT_ROOT"] . "/api/private/apps/029.exe";
+if (isset($_GET["Special"])) {
+    switch ($_GET["Special"]) {
+        case "XPTesterGC":
+            $xpTesters = [3, 115, 103, 118, 91, 102];
+            if (!in_array($user->getUserId(), $xpTesters)) {
+                Server::_404();
+            }
 
-if ($auth->isAuthed()) {
-    if (file_exists($file)) {
-        if (ob_get_level()) {
-            ob_end_clean();
-        }
-        header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
-        header('Content-Length: ' . filesize($file));
-        header('Pragma: public');
-        header('Cache-Control: must-revalidate');
-        header('Content-Disposition: attachment; filename="'.basename($file).'"');
-        readfile($file);
-        exit;
+            $file = $_SERVER["DOCUMENT_ROOT"] . "/api/private/apps/BoombloxXP.zip";
+            break;
     }
 }
 
-Server::_404();
-
+if (file_exists($file)) {
+    if (ob_get_level()) {
+        ob_end_clean();
+    }
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Length: ' . filesize($file));
+    header('Pragma: public');
+    header('Cache-Control: must-revalidate');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    readfile($file);
+    exit;
+}
 ?>
