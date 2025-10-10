ffb312b6ebdecd13ff696048a2deb92d3d26cad0
marsoc <boombloxkewl@gmail.com>
2025-10-09 19:15:33 -0500

    anticheat log updates

--- api/public/cheattest.php
+++ api/public/cheattest.php
@@ -4,7 +4,24 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
 global $user, $auth;
 !$auth->isAuthed() && Server::_404();
-$thing = $_GET["issue"];
-Discord::sendWebhookMessage("weird", "{$user->getUsername()} tried to exploit, clue: $thing");
-echo 1;
+if (isset($_GET["issue"])) {
+    $thing = $_GET["issue"];
+    Discord::sendWebhookMessage("weird", "{$user->getUsername()} tried to exploit, clue: $thing");
+    exit;
+} elseif (isset($_GET["dllissue"])) {
+    $issue = $_GET["dllissue"];
+    if (isset($_GET["module"])) {
+        $module = $_GET["module"];
+        if ($issue == "Unauth") {
+            Discord::sendWebhookMessage("weird", "{$user->getUsername()} tried to start the client with unauthorized module in folder: $module");
+            exit;
+        } elseif ($issue == "NewModule") {
+            Discord::sendWebhookMessage("weird", "{$user->getUsername()} tried to exploit with: $module");
+            exit;
+        } elseif ($issue == "ManualMap") {
+            Discord::sendWebhookMessage("weird", "{$user->getUsername()} tried to manual map and exploit with: $module");
+            exit;
+        }
+    }
+}
 ?>
