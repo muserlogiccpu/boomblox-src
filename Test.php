c6d8b5fb7a119e17d82ae29d771fb78c1d410bb9
marsoc <boombloxkewl@gmail.com>
2025-10-11 09:29:40 -0500

    update

--- Test.php
+++ Test.php
@@ -2,6 +2,6 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
 !$auth->isAuthed() && Server::_404();
 
-$user = new User(91);
-echo count($user->getFriends(false))
+#$user = new User(91);
+#echo count($user->getFriends(false))
 ?>
