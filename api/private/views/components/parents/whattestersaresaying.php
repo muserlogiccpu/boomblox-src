bc1845836fd7310e81871b6c5f5285cc6bdba719
marsoc <boombloxkewl@gmail.com>
2025-11-02 00:07:39 -0500

    added player reviews

--- api/private/views/components/parents/whattestersaresaying.php
+++ api/private/views/components/parents/whattestersaresaying.php
@@ -33,6 +33,13 @@ global $theme;
 				<strong>hexagon3d</strong>
 				<br> "pretty accurate but xp and vista support when"
 			</p>
+			<p>
+				<strong>duud6</strong>
+				<br> "Boomblox is a very good recreation of what Roblox during 2008 would look like, from the website
+to the client, it accurately resembles pretty much everything, even if its missing some things. The 
+community is amazing, many people are very awesome and kind and are way more nicer and friendly than other revival I have
+played, and plus people are actually pretty active here! I highly recommend joining and giving Boomblox a try!"
+			</p>
 		</blockquote>
 		<h3>
 			<a href="#2025">2025 Users</a>
@@ -50,6 +57,77 @@ global $theme;
 			<p>
 				<strong>jur</strong>
 				<br> "Boomblox is an actual revival, that gives you feel like how it is was in 2008, not like other revivals like pekora, finobe and other revivals made with custom website and filled with stupid accessories that doesnt make any sense. Boomblox is a good alternative to goodblox (original goodblox, not yomi's clones and others). In my opinion revival should revive the feel of how is it back then in roblox, and boomblox does this job perfectly, even tho its not fully finished."
+			</p>
+			<p>
+				<strong>jamster</strong>
+				<br> "Boomblox in my opinion is a great 2008 revival, the website seems pretty accurate as well as the client with the toolbox and everything, the community has some pretty talented folks and lots of active people which I think is awesome, and this has so much work put into it that this revival may be the closest thing we have to truly experiencing what it felt like playing roblox in 2008 even if it has some unfinished stuff that’s needs work, 9/10. 
+"
+			</p>
+			<p>
+				<strong>G2HJS</strong>
+				<br> "I have been in the ORC for a long time already and it's been a long while since I've seen a revival like Boomblox. The community is very united with genuine people, marsoc is doing all his best to make it as secure and accurate as he can and altho 2008 revivals/clients get boring after a week, this revival is only thriving. Hats down 
+"
+			</p>
+			<p>
+				<strong>Pwner Argentino</strong>
+				<br> "To be honest, Boomblox is pretty unique, the community is pretty unique, it's a calm breath after this whole year of only ecsr/syntax source code revivals, it's community is less toxic than any other i knew, the only problem i would find is [firebrand.] But after all, everything is good, i recommend join."
+			</p>
+			<p>
+				<strong>grenbiguy</strong>
+				<br> "Boomblox is honestly a very good 2008 ROBLOX revival. It has a working toolbox that I've never seen in a revival before, great games that are original than other revivals that are slop, and the community is very cool! I'd love to see more events and definetly would love to meet more people here."
+			</p>
+			<p>
+				<strong>nox</strong>
+				<br> "This is By far one of the most accurate revivals I played ever and one of the best, I haven't seen a revival so accurate for so long. Besides that the community is actually very good and the little things like currency giveaways and mini events are also great. I been here since the beginning of 2025 and this revival is definitely worth playing
+
+
+Another thing that hasn't been mentioned alot is the security within boomblox, the strict rules made by the staff. Not only in the server but within the security of the client. Ive never seen 2008 this secure in years.
+
+This is a 10/10 in my books, definitely one of the best revivals of this year and for 2008. If you have a friend looking for a 2008 revival id recommend boomblox. - The Podie"
+			</p>
+			<p>
+				<strong>MaskotGame</strong>
+				<br> "I think Boomblox is a pretty neat revival. Has good games on it and good avatar items too. I think it's the most high quality and secure pre-2010 revival you can find as of right now. I would very definitely recommend Boomblox for people if they are interested in 2008 Roblox."
+			</p>
+			<p>
+				<strong>quote256</strong>
+				<br> "Boomblox is very nice. The community and moderation is very nice too, if i had somebody whos asking me for an good roblox revival. allthough some things are really expensive but i dont really care
+
+The site itself is very accurate and i really love roblox revivals with an accurate page and not just an usual finobe clone like novarin - I really hope that this revival will get really far and many people will come and have fun playing with old bricks. 
+
+Maybe we need something else... but i dont know what it could be, yet
+
+I give out an solid 10/10"
+			</p>
+			<p>
+				<strong>JohnDoe</strong>
+				<br> "[truncated for space saving] I would definetely give this a 10/10 score for this amazing revival, I would definetely recommend this to someone who's looking for a 2008 revival.
+
+Much love and respect - Jon"
+			</p>
+			<p>
+				<strong>idontknowwhat</strong>
+				<br> "I'm going to keep this short and sweet since literally most of the reviews are 3 paragraphs long. Boomblox is pretty solid, patches most stuff, would say the best 2008 revival, but I'll rate it an 8/10. The reason I didn't rate it a 10/10 is because of the inactive people here :skull:, and I don't really like 2008 revivals, 2012L is better."
+			</p>
+			<p>
+				<strong>Spookie17654</strong>
+				<br> "I enjoy the functionality for creators, as I do for the reliability of the website. Nothing so far goes haywire and I appreciate that ya can do so much with what comes as a janky client!
+
+You can really understand now how people spent hours in studio just placing down toolbox models. Nonetheless, very fun, and very reliable revival! The community is very nice here, the detail is visible, so far, nothing is bad at all. I'd reccomend this revival to anybody wanting to enlist."
+			</p>
+			<p>
+				<strong>Valahul</strong>
+				<br> "I have been informed about this new "review" system for players who have been here since last year or have joined much recently. I would like to take time and admit that this project is actually doing it's job at being a fair simulation of 2008 Roblox, and while I haven't played it much, since, well, the website is sometimes broken and the lack of time I have, I can say for a certain extent that the client is functional, albeit, sometimes there are some bugs, to say the least, but hey, it works like how it did, 16 years ago... But still, great revival nevertheless."
+			</p>
+			<p>
+				<strong>chicken</strong>
+				<br> "guys you should play boomblocks its pretty cool i think and has many cool video games and video game hats for cool people
+
+install boomblox now for 5 cool and 6 hamburger"
+			</p>
+			<p>
+				<strong>Supa</strong>
+				<br> "blox chilling, bone tingling, orc redefining, fast-deved retrogem"
 			</p>
 		</blockquote>
 	</div>
