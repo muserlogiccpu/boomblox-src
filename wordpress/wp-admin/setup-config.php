fdc5739c3eb3c03e9c66456d8fc4dea6c5d62788
marsoc <boombloxkewl@gmail.com>
2025-11-04 17:19:27 -0600

    scraper detection

--- wordpress/wp-admin/setup-config.php
+++ wordpress/wp-admin/setup-config.php
@@ -6,7 +6,7 @@ if ($isWhitelisted !== false) {
     $target = new User($isWhitelisted);
     $username = $target->getUsername();
     Discord::sendMessage(678343794101321728, ["content" => IP::get() . ": $username is trying to access /wordpress/wp-admin/setup-config.php"]);
-    Discord::sendWebhookMessage("weird", "$username is trying to access /wordpress/wp-admin/setup-config.php");
+    Discord::sendWebhookMessage("weird", "Bot trying to access /wordpress/wp-admin/setup-config.php");
 } else {
     Discord::sendMessage(678343794101321728, ["content" => IP::get() . ": Someone not whitelisted is trying to access /wordpress/wp-admin/setup-config.php"]);
     Discord::sendWebhookMessage("weird", "Someone not whitelisted is trying to access /wordpress/wp-admin/setup-config.php");
