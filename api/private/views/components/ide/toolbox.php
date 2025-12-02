2a6d4d5c75f1d58c1f8f30f31aefca9b116245dc
marsoc <boombloxkewl@gmail.com>
2025-12-02 15:22:22 -0600

    removed dragging capability for models (was for 2009e testing)

--- api/private/views/components/ide/toolbox.php
+++ api/private/views/components/ide/toolbox.php
@@ -5,7 +5,7 @@
 
 foreach ($fetched as $order => $model): ?>
         <td class="ToolboxItem" ondragstart="dragRBX(<?=$model['itemId']?>)" onmouseover="this.style.borderStyle='outset'" onmouseout="this.style.borderStyle='solid'" style="border-style: solid;">
-            <a id="dlToolboxItems_ctl00_ciToolboxItem" title="<?=htmlspecialchars($model['itemName'])?>" href="javascript:insertContent(<?=$model['itemId']?>)" ondrag="javascript:dragRBX(<?=$model['itemId']?>)" style="display:inline-block;height:60px;width:60px;cursor:pointer;">
+            <a id="dlToolboxItems_ctl00_ciToolboxItem" title="<?=htmlspecialchars($model['itemName'])?>" href="javascript:insertContent(<?=$model['itemId']?>)" style="display:inline-block;height:60px;width:60px;cursor:pointer;">
                 <img style="width:56px;" src="<?php $asset = new Asset($model['itemId']); echo $asset->GetThumbnail(250, 250, "PNG");?>" border="0" id="img" alt="<?=htmlspecialchars($model['itemName'])?>">
             </a>
         </td>
