ebe7ec3fa460f2c9f41dbf6bbb94cbac1ed15410
marsoc <boombloxkewl@gmail.com>
2025-12-04 20:46:30 -0600

    adbuy.aspx finalize concept

--- api/private/views/components/userad/buy.php
+++ api/private/views/components/userad/buy.php
@@ -1,18 +1,24 @@
 <?php
-global $db, $user;
+global $theme, $user;
 $adId = (int)$_GET["adId"];
 $ad = new UserAd($adId);
 ?>
 
 <div id="Body">
+    <div class="SearchBar">
+        <h4>You found an in-development page, wooo!</h4>
+    </div>
+    <h3 style="text-align: center;">Bid to Run an Ad</h3>
+    <div>For a detailed explanation of how advertising on <?=Site::getThemeProperty("alias", $theme)?> works, check out the <a id="ctl00_ctl00_cphRoblox_cphMyRobloxContent_AdHelp" href="http://wiki.roblox.com/index.php/How_Advertising_Works">How Advertising Works</a> article in the help section.</div>
     <img src="<?=$ad->getImage()?>">
     <div id="ctl00$cphRoblox$EstimatorPanel" style="padding-left: 5px; width: 200px; border: solid #000 1px; clear: both;">
         <h3>Traffic Estimator</h3>
         <p>This estimator uses data from ads run yesterday to guess how many impressions your ad will receive. <b>This is just a guess.</b> If other players spend a lot on ads today, you will see fewer impressions.</p>
         <br>
+        <p style="color: Red; display: none;">Estimated Impressions: 0</p>
     </div>
     <div style="margin-top: 5px;">
-        <span style="">Bid in Tix</span>
+        <span>Bid in Tix</span>
         <input type="text" class="TextBox"><br>
         <input type="submit" class="MediumButton" value="Place Bid">
         <input type="submit" class="MediumButton" value="Cancel">
