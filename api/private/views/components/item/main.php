d5efc74bdd15409edb34ec89c0f94fd2dbd64a71
marsoc <boombloxkewl@gmail.com>
2025-12-08 21:42:06 -0600

    ads now appear on catalog items

--- api/private/views/components/item/main.php
+++ api/private/views/components/item/main.php
@@ -12,7 +12,9 @@ if (isset($purchaseData)) {
             <?=PageBuilder::addComponent("item", "details", $packed);?>
             <?=PageBuilder::addComponent("item", "tabs", $packed);?>
         </div>
-        <div class="Ads_WideSkyScraper"></div>
+        <div class="Ads_WideSkyScraper">
+        <?=Ad::generateAd("160x600")?>
+        </div>
         <div style="clear:both"></div>
         <?=PageBuilder::addComponent("item", "purchasemodal", $packed);?>
     </div>
