b0b276fb2fd203cb3ca3fd675be36e71d7699c2a
marsoc <boombloxkewl@gmail.com>
2025-12-16 13:55:28 -0600

    improved edit.ashx error handling

--- Game/Edit.php
+++ Game/Edit.php
@@ -8,8 +8,15 @@ if (!$auth->isAuthed()) {
 }
 
 $placeId = $_GET["PlaceID"];
-$userId = ROBLOSECURITY::match($_COOKIE["BROBLOSECURITY"]);
-$user = new User($userId);
+if (!$user->ownsPlace($placeId)) {
+	$file = new File("/api/private/lua/joinfail.lua", [
+		"Error" => "edit place, this place is not yours."
+	]);
+	
+	echo $file->handle();
+	exit;
+}
+
 $uploadUrl = in_array($placeId, $user->getPlaces(true)) ? "http://".domain."/Data/Upload.ashx?id=" . $placeId : "";
 
 $file = new File("/api/private/lua/edit.lua", [
