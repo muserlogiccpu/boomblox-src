1cb5cae58947ad9d3bd2d764e3a135893359c79d
marsoc <boombloxkewl@gmail.com>
2025-12-27 14:43:04 -0600

    reset/tips css added

--- Catalog.php
+++ Catalog.php
@@ -24,8 +24,22 @@ $page->buildHeader();
 <div id="Body">
 	<div id="CatalogContainer">
 		<div id="SearchBar" class="SearchBar">
-			<span class="SearchBox"><input name="SearchTextBox" type="text" maxlength="100" class="TextBox" value="<?=$q?>"/></span>
+			<span class="SearchBox"><input name="SearchTextBox" type="text" maxlength="100" class="TextBox" value="<?=htmlspecialchars($q)?>"/></span>
 			<span class="SearchButton"><input type="submit" name="SearchButton" value="Search"/></span>
+			<?php if ($theme !== 3): ?>
+			<span class="SearchLinks"><sup><a id="ctl00_cphRoblox_rbxCatalog_ResetSearchButton" href="javascript:__doPostBack('ctl00$cphRoblox$rbxCatalog$ResetSearchButton','')">Reset</a>&nbsp;|&nbsp;</sup><a href="#"><sup>Tips</sup>
+            <span>Exact Phrase: "red brick"<br>
+                  Find ALL Terms: red and brick =OR=  red + brick<br>
+                  Find ANY Term: red or brick =OR= red | brick<br>
+                  Wildcard Suffix: tel* (Finds teleport, telamon, telephone, etc.)<br>
+                  Terms Near each other: red near brick =OR= red ~ brick<br>
+                  Excluding Terms: red and not brick =OR= red - brick<br>
+                  Grouping operations: brick and (red or blue) =OR= brick + (red | blue)<br>
+                  Combinations: "red brick" and not (tele* or tower) =OR= "red brick" - (tele* | tower)
+                  Wildcard Prefix is NOT supported: *port will not find teleport, airport, etc.
+            </span>
+			<?php endif; ?>
+        </a></span>
 		</div>
 		<?=$catalog->loadSorts()?>
 		<div class="Assets">
