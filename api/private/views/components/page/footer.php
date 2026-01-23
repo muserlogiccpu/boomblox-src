e37ecf790a15a13fe85e76bce2748eb86e3b5770
marsoc <boombloxkewl@gmail.com>
2026-01-22 22:36:42 -0600

    updated theme manager to account for 2026

--- api/private/views/components/page/footer.php
+++ api/private/views/components/page/footer.php
@@ -3,8 +3,8 @@
                     <p class="Legalese">
                     <?=Site::getThemeProperty("alias",$theme); ?>, "Online Building Toy", characters and names are under 
                         <a href="/info/About.aspx"><?=Site::getThemeProperty("company",$theme); ?></a>,
-                        <?=Site::getThemeProperty("year",$theme);?><br/>
-                        <?=Site::getThemeProperty("company2",$theme);?> is not affiliated with Lego, MegaBloks, Bionicle, Pokemon, Nintendo, Lincoln Logs, Yu Gi Oh, K'nex, Tinkertoys, Erector Set, or the Pirates of the Caribbean. ARrrr!
+                        <?=Site::getThemeProperty("year", $theme);?><br/>
+                        <?=Site::getThemeProperty("company2", $theme);?> is not affiliated with Lego, MegaBloks, Bionicle, Pokemon, Nintendo, Lincoln Logs, Yu Gi Oh, K'nex, Tinkertoys, Erector Set, or the Pirates of the Caribbean. ARrrr!
                         <br>
                         Use of this site signifies your acceptance of the
                         <a href="/info/TermsOfService.aspx">Terms and Conditions</a>.
