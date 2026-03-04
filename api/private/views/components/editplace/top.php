8ff7639d590088a5038fdf48f5d000393e6747d7
marsoc <boombloxkewl@gmail.com>
2026-03-04 07:48:17 -0600

    save message when updating an item now shows for places

--- api/private/views/components/editplace/top.php
+++ api/private/views/components/editplace/top.php
@@ -1,3 +1,9 @@
 <div id="Body">
     <div id="ConfigurePlaceContainer">
         <h2>Configure Place</h2>
+        
+        <?php if (isset($_POST['__EVENTTARGET'])): if ($_POST['__EVENTTARGET'] == 'ctl00$cphRoblox$lbSubmit'): ?>
+        <div id="EditItemContainer">
+            <div id="Confirmation" class="Suggestion" style="font-size: 12px;">Your changes to the item have been saved. (<?=date("h:i:s A")?>)</div>
+        </div>
+        <?php Server::_self(3); endif; endif; ?>
