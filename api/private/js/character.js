a6025c1d2ac5b6847056cf3f50e842bded5ce4c3
marsoc <boombloxkewl@gmail.com>
2026-03-15 12:09:39 -0500

    you can now change body colors in earlier browsers

--- api/private/js/character.js
+++ api/private/js/character.js
@@ -1,13 +1,4 @@
-function isIDE() 
-{
-    return window.external.IsRobloxAppIDE;
-}
-
-if (isIDE()) {
-    window.onload = function() {
-
-    };
-} else {
+if (typeof document.addEventListener === "function") {
     document.addEventListener("DOMContentLoaded", function() {
         $('.ColorPickerItem').click(function(event) {
             $("input[name='__EVENTARGUMENT']").val($(event.target).css("background-color"));
@@ -21,4 +12,18 @@ if (isIDE()) {
             $(".popupControl").attr("title", event.target.id);
         });
     });
+} else {
+    window.onload = function() {
+        $('.ColorPickerItem').click(function(event) {
+            $("input[name='__EVENTARGUMENT']").val($(event.target).css("background-color"));
+            $("input[name='__EVENTTARGET']").val("Color$"+$(".popupControl").attr("title"));
+            $("form[name='aspnetForm']").submit();
+        });
+        $('.BodyPart').click(function(event) {
+            $(".popupControl").css("left", event.clientX+"px");
+            $(".popupControl").css("top", event.clientY+"px");
+            $(".popupControl").css("visibility", "visible");
+            $(".popupControl").attr("title", event.target.id);
+        });
+    };
 }
--- info/TermsOfService.php
+++ info/TermsOfService.php
@@ -1,7 +1,7 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/api/private/core/main.php";
 global $theme, $auth, $user;
-!$auth->isAuthed() && Server::_404();
+
 
 $page = new PageBuilder(Site::getThemeProperty("alias",$theme) . " Login", $theme, "/templates/dryheader.php");
 $page->buildHeader();
